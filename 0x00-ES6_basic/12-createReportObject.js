9915bea02e3a7165e9f8338e1f10662e230b77ae
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2024-06-26 13:31:06 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: {
-      ...employeesList
+      ...employeesList,
     },
     getNumberOfDepartments(list) {
       return Object.keys(list).length;
